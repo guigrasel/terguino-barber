da78e4e93997face261c66f07a67dd73734090db
Guilherme Grasel <guigrasel@gmail.com>
2026-06-25 20:29:26 -0300

    ✨ Feat: Cria estrutura base do front

---
+++ src/constants/routes.js
@@ -0,0 +1,15 @@
+export const APP_ROUTES = {
+  client: {
+    home: '/',
+    newAppointment: '/agendamento',
+    myAppointments: '/meus-agendamentos',
+    confirmation: '/confirmacao',
+  },
+  admin: {
+    dashboard: '/admin',
+    professionals: '/admin/profissionais',
+    services: '/admin/servicos',
+    schedule: '/admin/agenda',
+    history: '/admin/historico',
+  },
+}
